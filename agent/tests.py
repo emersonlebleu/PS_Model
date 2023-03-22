801af727b6a086d8d263287b3261156adf77ec24
Emerson <45885321+emersonlebleu@users.noreply.github.com>
2023-03-22 12:27:12 -0600

    Test Agent
    Testing setup for agent.

--- agent/tests.py
+++ agent/tests.py
@@ -8,7 +8,7 @@ agent.add_clip_to_memory(clip=["sad"])
 agent.clear_log()
 
 reward = 0
-for i in range(10):
+for i in range(20):
     random_percept = random.choice([["happy"], ["sad"]])
     action = agent.observe_environment(observations=random_percept, reward=reward)
     if random_percept == ["happy"] and action == "+":
